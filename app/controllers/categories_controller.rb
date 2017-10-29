bdd0b4d2b5e4ac6317d9574035733427109d8178
Nichanan Kesonpat <n.kesonpat@gmail.com>
2017-10-29 18:33:05 +0000

    Added ability to edit categories, displayed in article listing

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -17,7 +17,20 @@ class CategoriesController < ApplicationController
         else
             render 'new'
         end
+    end
     
+    def edit
+        @category = Category.find(params[:id])
+    end
+    
+    def update
+        @category = Category.find(params[:id])
+        if @category.update(category_params)
+            flash[:success] = "Category name was successfully updated."
+            redirect_to categories_path(@category)
+        else
+            render 'edit'
+        end
     end
     
     def show
