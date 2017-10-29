ab6bb444d41b33befeff0629a0cfac40606b16a3
Nichanan Kesonpat <n.kesonpat@gmail.com>
2017-10-29 12:28:09 +0000

    Added user profile page UI with image and user article listing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,24 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
     
     private
     
