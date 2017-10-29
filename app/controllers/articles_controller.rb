5f34178a60ba44a36bf60fe4910d8894ad74e826
Nichanan Kesonpat <n.kesonpat@gmail.com>
2017-10-29 14:58:50 +0000

    Added user restrictions at the UI and controller level + bug fixes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
     before_action :set_article, only: [:edit, :update, :show, :destroy]
+    before_action :require_user, except: [:index, :show]
+    before_action :require_same_user, only: [:edit, :update, :delete]
     
     def index
         @articles = Article.paginate(:page => params[:page], :per_page => 5)
@@ -10,9 +12,8 @@ class ArticlesController < ApplicationController
     end
     
     def create
-        debugger
         @article = Article.new(article_params)
-        @article.user = User.first #Temporary hard code to not worry about creating accounts yet
+        @article.user = current_user
         if @article.save
             flash[:success] = "New article successfully created."
             redirect_to article_path(@article)
@@ -51,4 +52,12 @@ class ArticlesController < ApplicationController
     def article_params
         params.require(:article).permit(:title, :description)
     end
+    
+    def require_same_user
+        if current_user != @article.user
+            flash[:danger] = "You can only edit or delete your own articles."
+            redirect_to root_path
+        end
+    end
+    
 end
